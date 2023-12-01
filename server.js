95c6bd69771bd6f8a7db7a736c62506d86fefc05
schaererPanda <114098546+schaererPanda@users.noreply.github.com>
2023-11-30 19:11:32 -0800

    Update

--- server.js
+++ server.js
@@ -10,14 +10,6 @@ config();
 const PORT = process.env.PORT;
 const app = express();
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
-//app.use(cors())
-PORT = 3000;
-
 /**
  * @typedef {{
  *  email: string,
